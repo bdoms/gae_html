efda8de7db020bed5abc5de0b4783f6303f8d399
bdoms <ravenize@gmail.com>
2015-01-23 16:03:23 -0600

    Simplified how this works so that only a single decorator is used.

--- __init__.py
+++ __init__.py
@@ -9,52 +9,38 @@ from htmlmin import HTMLMinifier
 DEBUG = os.environ.get('SERVER_SOFTWARE', '').startswith('Development')
 
 
+# decorator to skip straight to the cached version, or cache it if it doesn't exist
+def cacheAndRender(expires=86400, minify=True, include_comments=False, use_datastore=False):
+    minifier = None
+    if minify:
+        minifier = HTMLMinifier(include_comments=include_comments)
 
-def cacheHTML(controller, function, **kwargs):
-    key = controller.request.path + controller.request.query_string
-    html = memcache.get(key)
-
-    if not html:
-        use_datastore = kwargs.get("use_datastore", False)
-        if use_datastore:
-            html = getFromDatastore(key)
-
-    if not html:
-        html = function()
-
-        # don't cache if in development or for admins
-        if not DEBUG and not users.is_current_user_admin():
-            # cache for 1 day by default
-            expires = kwargs.get("expires", 86400)
-            minify = kwargs.get("minify", True)
-            if minify:
-                include_comments = kwargs.get("include_comments", False)
-                minifier = HTMLMinifier(include_comments=include_comments)
-                minifier.feed(html)
-                html = minifier.close()
-
-            memcache.add(key, html, expires)
-
-            if use_datastore:
-                html_cache = HTMLCache(id=key, html=html, expires=expires)
-                html_cache.put()
-
-    return html
-
-
-# decorator to skip straight to the cached version
-def renderIfCached(use_datastore=False):
     def wrap_action(action):
         def decorate(*args,  **kwargs):
             controller = args[0]
             key = controller.request.path + controller.request.query_string
             html = memcache.get(key)
+
             if not html and use_datastore:
                 html = getFromDatastore(key)
-            # don't serve a cached version in development or to admins
-            if html and not DEBUG and not users.is_current_user_admin():
-                return controller.response.out.write(html)
-            return action(*args, **kwargs)
+
+            if not html:
+                html = action(*args, **kwargs)
+
+            if html:
+                if minifier:
+                    minifier.feed(html)
+                    html = minifier.close()
+
+                # don't cache if in development or for admins
+                if not DEBUG and not users.is_current_user_admin():
+                    memcache.add(key, html, expires)
+
+                    if use_datastore:
+                        html_cache = HTMLCache(id=key, html=html, expires=expires)
+                        html_cache.put()
+
+                controller.response.out.write(html)
         return decorate
     return wrap_action
 
